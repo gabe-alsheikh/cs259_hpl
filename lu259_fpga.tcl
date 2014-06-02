c63d973d5ccd895d60372e872d8ce1e5ea1f2fe5
gabe-alsheikh <gm.alsheikh@gmail.com>
2014-06-01 17:38:07 -0700

    Create lu259_fpga.tcl

---
+++ lu259_fpga.tcl
@@ -0,0 +1,28 @@
+# Create Project 
+create_project -name lu259_fpga -platform zc706-linux-uart -force
+
+# Add host code
+add_host_src -filename "lu259_cl.c" -type source
+
+# Create a kernel for the matrix multiplication
+create_kernel -id LUFact -type clc
+add_kernel_src -id LUFact -filename "lu259_cl.cl"
+
+# Create a Xilinx OpenCL Kernel Binary Container
+create_xclbin lu259_fpga
+
+# Select the execution target of the kernel
+# EDIT: -N 8 parameter added in 
+map_add_kernel_instance -xclbin lu259_fpga -id LUFact -target fpga0:OCL_REGION_0
+
+# Compile the host code
+compile_host -arch arm -cflags "-O3 -Wall -D FPGA_DEVICE -D TEST"
+
+# Build the system
+#build_estimate
+build_system
+
+# Package SD Card Image
+#build_sdimage
+
+exit
